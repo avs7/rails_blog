040b893fd85ce0038e335f8d3cea9b880dfb1439
Alex Slyusarev <aslyusarev@gmail.com>
2023-05-03 12:48:26 -0700

    added hardcoded http_basic_auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  http_basic_authenticate_with name: "alex", password: "abc123", except: [:index, :show]
   def index
     @articles = Article.all
   end
